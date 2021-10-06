5266cc4c3e62dbb40667fa0aa39ec2274a67535c
AhmEeAli2003 <68246583+AhmEeAli2003@users.noreply.github.com>
2021-10-06 16:32:53 +0200

    Update grades.c

--- grades.c
+++ grades.c
@@ -1,20 +1,108 @@
 #include<stdio.h>
-#include<stdlib.h>
 void main(void)
 {
-    int grade,i=0,j=0,k=0;
-    int temp[10];
-    printf("Input grades: "); scanf("%d",&grade);
-    while(grade>0)
+    float grade = 0.0;
+    float total = 0.0;
+    float average = 0.0;
+    char ch;
+    int countg = 0;
+    float f_num = 0.0;
+    int temp;
+    printf("Compute the average grades.\n");
+    Loop:
+        printf("Enter grade (Esc to terminate): ");
+        int counter  = 0;
+        int countf = 0;
+        int i_num = 0;
+    while((ch=getch()) != 27)
     {
-        temp[i]=grade%10;
-        if (temp[i] == 5) j++;
-        else if (temp[i] == 2) k++;
-        else exit(0);
-        grade/=10;
-        i++;
+            switch(ch)
+            {
+            case '0'...'9':
+                putch(ch);
+                i_num = i_num*10+ch-'0';
+                counter++;
+                break;
+            case 8:
+                if(counter > 0)
+                {
+                    if(counter == 1) i_num = 0;
+                    else temp = i_num % 10;
+                    putch('\b');
+                    putch(' ');
+                    putch('\b');
+                    counter--;
+                }
+                i_num = (i_num-temp)/10;
+                break;
+            case '.':
+                putch(ch);
+                while((ch=getch()))
+            {
+            switch(ch)
+            {
+            case '0'...'9':
+                putch(ch);
+                i_num = i_num*10+ch-'0';
+                countf++;
+                break;
+            case 8:
+                if(counter > 0)
+                {
+                    if(counter == 1) i_num = 0;
+                    else temp = i_num % 10;
+                    putch('\b');
+                    putch(' ');
+                    putch('\b');
+                    countf--;
+                }
+                i_num = (i_num-temp)/10;
+                break;
+            case '\r':
+                f_num = i_num;
+                while(countf > 0)
+                {
+                f_num /= 10;
+                countf--;
+                }
+                grade = f_num;
+                countg++;
+                total += grade;
+                printf("\n");
+                goto Loop;
+                break;
+            case 27:
+                goto Res;
+                break;
+            default:
+                putch(ch);
+                printf("\nInvalid input\n");
+                goto Loop;
+                break;
+            }
+        }
+            case '\r':
+                f_num = i_num;
+                while(countf > 0)
+                {
+                f_num /= 10;
+                countf--;
+                }
+                grade = f_num;
+                countg++;
+                total += grade;
+                printf("\n");
+                goto Loop;
+                break;
+            default:
+                putch(ch);
+                printf("\nInvalid input\n");
+                goto Loop;
+                break;
     }
-    if(j==k) printf("=");
-    else if(j>k) printf("5");
-    else if(j<k) printf("2");
+    }
+    Res:
+        average = total / countg;
+        printf("\n");
+        printf("%.2f\n",average);
 }
